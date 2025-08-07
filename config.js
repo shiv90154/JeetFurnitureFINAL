9c9ba124aac85e8ff44f7595d36268dea8001acc
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-08-07 12:07:02 +0530

    config+axiosinstance

---
+++ config.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://fvvcbrpm-4000.inc1.devtunnels.ms/'
+
+export default API_URL;
---
+++ src/AxiosInstance.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import API_URL from "../config";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("userToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const setupAxiosInterceptors = (navigate, params) => {
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+        toast.error("Session expired. Please sign in again.");
+        sessionStorage.clear();
+        const org_id = params?.org_id;
+        if (org_id) {
+          navigate(`/client/${org_id}/login`);
+        } else {
+          navigate("/superadmin/login");
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+};
+
+export default axiosInstance;
