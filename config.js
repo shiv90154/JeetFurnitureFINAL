5dfce84fb058f413b7704f1d61cdff79202ebbf0
Nikhil Kumar <nik326990@gmail.com>
2025-08-30 15:42:34 +0530

    cj

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 // const API_URL = 'https://fvvcbrpm-4000.inc1.devtunnels.ms/'
 // const API_URL = 'https://www.chauhansonsjewellers.com/'
-const API_URL = 'http://31.97.237.55:4000'
+// const API_URL = 'http://31.97.237.55:4000'
+const API_URL = 'https://chauhansonsjewellers.com/';
+
 
 export default API_URL;
